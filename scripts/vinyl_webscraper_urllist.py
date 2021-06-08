8b06e7f61adf841416190e07f19e7c7ef13f541c
1602077 <j.munday.1@warwick.ac.uk>
2021-06-08 22:41:08 +0100

    testing complete of functional implimentation of webscraper

--- scripts/vinyl_webscraper_urllist.py
+++ scripts/vinyl_webscraper_urllist.py
@@ -92,7 +92,7 @@ best_price = [0 for _ in range(size)]
 previous_price = [0 for _ in range(size)]
 
 try:
-    records_df = pd.read_csv('records.csv')
+    records_df = pd.read_csv('../output_data/records.csv')
     best_price = records_df['Best Price'].tolist()
     previous_price = records_df['Current Price'].tolist()
     assert len(best_price) == len(title), "Mismatched number of records in url.txt to that stored in records.csv"
@@ -105,55 +105,27 @@ print("\n>>> Fetching Record Prices")
 # SCRAPPING DATA
 ##################################################################
 for indx, url in enumerate(urls):
-   # driver.get(url)
-   # soup = BeautifulSoup(driver.page_source, features="html.parser")
-
-   # artist_tmp = soup.find('span', attrs={'class': 'author notFaded'}).get_text()
-   # artist_tmp  = artist_tmp.split('\n')
-   # if type(artist_tmp[1])==str:
-   #     artist[indx] = artist_tmp[1]
-
-   # name = soup.find('title').get_text()
-   # if "[VINYL]" in name:
-   #     name, _ = name.split(' [VINYL]')
-   # if artist[indx] in name:
-   #     name = name.strip(artist[indx])
-   #     if not name:
-   #         name = "Self Titled"
-   # title[indx] = name
-
-   # price = soup.find('span', id="price_inside_buybox")
 
     #TODO: ADJUST TO INCLUDE FUNCTIONAL WEBSCRAPER DEFINED ABOVE, THIS WILL IMPROVE CLARITY OF WHAT IS GOING ON HERE
     #TODO: ADD ANOTHER SECTION TO CODE THAT WOULD KEEP TRACK OF THE HISTORICAL DATA FOR ALL RECORDS IN WISHLIST
     artist_name, record_name, price = records_wishlist_scraper(driver, url)
+    artist[indx] = artist_name
+    title[indx] = record_name
+
     if price is not None:
-   #     price = price.get_text()
-   #     price = price.strip('\n')
-   #     price = float(price.strip('£'))
-        artist[indx] = artist_name
-        title[indx] = record_name
+
         current_price[indx] = price
-
-        if previous_price[indx] == 0:
-            price_change[indx] = 0
-
+        price_change[indx] = price - previous_price[indx]
+        
         if best_price[indx] == 0:
             best_price[indx] = price
 
-        if price < previous_price[indx]:
-            price_change[indx] = -100*(previous_price[indx] - price) / price
-            if price < best_price[indx]:
-                best_price[indx] == price
+        if price < best_price[indx]:
+            best_price[indx] = price
 
-        #if price == current_price:
-        #    price_change[indx] = 0
-
-        if price >= previous_price[indx] and previous_price[indx] != 0:
-            price_change[indx] = 100*(price - previous_price[indx]) / previous_price[indx]
 
 driver.quit()
-price_change = [str(round(elem, 1)) for elem in price_change]
+price_change = [str(round(elem, 2)) for elem in price_change]
 
 print(">>> Printing Prices\n")
 
@@ -161,13 +133,13 @@ print(">>> Printing Prices\n")
 # OUTPUT
 ##################################################################
 pd.set_option("display.max_rows", None, "display.max_columns", None, 'display.expand_frame_repr', False)
-df = pd.DataFrame({'Artist': artist, 'Title': title, 'Best Price': best_price, 'Previous Price': previous_price, 'Current Price': current_price,'Price Change (%)': price_change})
+df = pd.DataFrame({'Artist': artist, 'Title': title, 'Best Price': best_price, 'Previous Price': previous_price, 'Current Price': current_price,'Price Change (£)': price_change})
 
 # left align df
 df.style.set_properties(**{'text-align': 'left'}).set_table_styles([ dict(selector='th', props=[('text-align', 'left')])])
 df.to_csv('../output_data/records.csv', index=False, encoding='utf-8')
 
 df = df[df['Current Price'] != 0]
-terminal_output = df.sort_values(['Price Change (%)'], ascending=True).to_string(index=False)
+terminal_output = df.sort_values(['Artist'], ascending=True).to_string(index=False)
 print(terminal_output)
 
